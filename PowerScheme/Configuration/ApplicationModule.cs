9560a1ccc1545e8a52dff62ac3d7be725544b357
MACE <d.mace@live.com>
2020-07-11 21:45:14 +0300

    + added Ninject

---
+++ PowerScheme/Configuration/ApplicationModule.cs
@@ -0,0 +1,14 @@
+﻿using Ninject.Modules;
+using PowerScheme.Services;
+
+namespace PowerScheme.Configuration
+{
+    public class ApplicationModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<FormMain>().ToSelf().InSingletonScope();
+            Bind<IPowerSchemeService>().To<PowerSchemeService>().InSingletonScope();
+        }
+    }
+}
---
+++ PowerScheme/Configuration/CompositionRoot.cs
@@ -0,0 +1,20 @@
+﻿namespace PowerScheme.Configuration
+{
+    using Ninject;
+    using Ninject.Modules;
+
+    public class CompositionRoot
+    {
+        private static IKernel _ninjectKernel;
+
+        public static void Wire(INinjectModule module)
+        {
+            _ninjectKernel = new StandardKernel(module);
+        }
+
+        public static T Resolve<T>()
+        {
+            return _ninjectKernel.Get<T>();
+        }
+    }
+}
